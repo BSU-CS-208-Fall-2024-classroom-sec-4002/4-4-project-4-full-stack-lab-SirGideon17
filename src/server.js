4be9f54ef3d59b2d33e53512cf70e0430f441e14
Simeon Grant <tresure.hunter7@gmail.com>
2024-11-04 09:04:37 -0700

    C2 - style.css done

--- src/server.js
+++ src/server.js
@@ -11,30 +11,52 @@ const db = new sqlite3.Database(':memory:')
 db.run(`CREATE TABLE todo (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     task TEXT NOT NULL)`)
-
+   
 const app = express()
 app.use(express.static('public'))
 app.set('views', 'views')
 app.set('view engine', 'pug')
 app.use(express.urlencoded({ extended: false }))
 
+
+
 app.get('/', function (req, res) {
     //TODO You will need to do a SQL select here
     //TODO You will need to update the code below!
     console.log('GET called')
     res.render('index')
+    const local = { tasks: [] }
+    db.each('SELECT id, task FROM todo', function (err, row) {
+        if (err) {
+        console.log(err)
+        } else {
+        local.tasks.push({ id: row.id, task: row.task })
+        }
+    }, function (err, numrows) {
+        if (!err) {
+        res.render('index', local)
+        } else {
+        console.log(err)
+        }
+  })
 
 })
 
 app.post('/', function (req, res) {
     console.log('adding todo item')
     //TODO You will need to to do a SQL Insert here
+    const stmt = db.prepare('INSERT INTO todo tasks VALUES ')
+    stmt.run(req.body.todo)
+    stmt.finalize()
 
 })
 
 app.post('/delete', function (req, res) {
     console.log('deleting todo item')
     //TODO you will need to delete here
+    const stmt = db.prepare('DELETE FROM todo where id = (?)')
+    stmt.run(req.body.id)
+    stmt.finalize()
 
 })
 
